927da91bcb64d6eb64cd8fc59b17e78c8f8e3c0b
James Nguyen <66525324+jamesnguyenn@users.noreply.github.com>
2023-11-25 16:28:36 +0700

    Add Base Repository

--- src/James.Blog.Repository/IRepository/Mongo/IMGBaseRepository.cs
+++ src/James.Blog.Repository/IRepository/Mongo/IMGBaseRepository.cs
@@ -4,8 +4,5 @@ namespace James.Blog.Repository.IRepository.Mongo;
 
 public interface IMGBaseRepository<T>
 {
-    public FilterDefinition<T> Filter { get; }
     Task AddEntity(T entity);
-    Task Upsert(List<T> entities);
-    Task AddManyAsync(List<T> entities);
 }
--- src/James.Blog.Repository/Repository/Mongo/MGBaseRepository.cs
+++ src/James.Blog.Repository/Repository/Mongo/MGBaseRepository.cs
@@ -1,8 +1,21 @@
+using James.Blog.Repository.IRepository.Mongo;
+using James.Blog.Repository.MongoInfrastructure;
 using MongoDB.Driver;
-
 namespace James.Blog.Repository.Repository.Mongo;
 
 public abstract class MGBaseRepository<T> : IMGBaseRepository<T>
 {
+
     private readonly IMongoCollection<T> _collection;
+
+    protected MGBaseRepository(IMongoDb database)
+    {
+        _collection = database.Db.GetCollection<T>(typeof(T).Name);
+    }
+
+    public virtual Task AddEntity(T entity)
+    {
+        return _collection.InsertOneAsync(entity);
+    }
+
 }
